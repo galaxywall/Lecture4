90aaf5aa9d9cfd0328e7e9db5140e8e851f5b279
galaxywall <harvey961001@gmail.com>
2020-10-11 15:43:23 -0500

    Removing the line i=i+1 within the for loop

--- buggy_code.R
+++ buggy_code.R
@@ -6,7 +6,6 @@ investment_value<-function(x,y,z=0.95, different_years=c(3,5)){
   different_multiplier = 1 + z
   beginning_x=x
   for(i in 1:10){
-    i=i+1
     if (!(i %in% different_years)){      # should be true unless i is in the different_years vector
       x<-beginning_x*multiplier
     } else {
